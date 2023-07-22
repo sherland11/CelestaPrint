9de797cdd888bbef04cd7944983f1407590c70bf
sherland <vancklies@gmail.com>
2023-07-22 23:40:11 +0300

    ДОбавление заголовка и изображений в Clients

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Clients() {
     return (
-        <div>Clients</div>
+        <Container className="clients">
+            <Row className="clients__title">
+                <Col sm='12' className="caption">НАШИ КЛИЕНТЫ</Col>
+            </Row>
+
+            <Row className="clients__logo">
+                <Col sm='2'><img src="images/clients-logo/1.jpg" alt="Crop and Highlight"/></Col>
+                <Col sm='2'><img src="images/clients-logo/2.jpg" alt="N"/></Col>
+                <Col sm='2'><img src="images/clients-logo/3.jpg" alt="Miiissy"/></Col>
+                <Col sm='2'><img src="images/clients-logo/4.jpg" alt="Peppermint"/></Col>
+                <Col sm='2'><img src="images/clients-logo/5.jpg" alt="Pixel Labs"/></Col>
+                <Col sm='2'><img src="images/clients-logo/6.jpg" alt="Marnie"/></Col>
+            </Row>
+        </Container>
     )
 }
