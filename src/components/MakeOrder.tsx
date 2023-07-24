0792386467f5f8b5500f2dbc5c20892b4b7991de
sherland <vancklies@gmail.com>
2023-07-25 00:17:33 +0300

    добавлены три первые формы в makeOrder

--- src/components/MakeOrder.tsx
+++ src/components/MakeOrder.tsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
-export default function MakeORder() {
+export default function MakeOrder() {
     return (
         <Container className="makeOrder">
             <Row className="makeOrder__title">
-                <Col></Col>
-                <Col></Col>
+                <Col className="caption" sm="12">СДЕЛАЙТЕ ЗАКАЗ</Col>
+                <Col className="desc" sm="8">Отправьте контакты, информацию о своем заказе и загрузите макет. Мы свяжемся с вами для подтверждения и уточнения деталей заказа.</Col>
             </Row>
 
             <Row className="makeOrder__form">
-                <Col></Col>
+                <Col sm="12">
+                    <Form>
+                        <Form.Group>
+                            <Form.Control type="text" placeholder="Ваше имя" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control type="email" placeholder="Ваш email" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control as="textarea" rows={1} placeholder="Комментарий к заказу" />
+                        </Form.Group>
+                        <Form.Group></Form.Group>
+                        <Form.Group></Form.Group>
+                        <Button type="submit">Submit</Button>
+                    </Form>
+                </Col>
             </Row>
         </Container>
     )
