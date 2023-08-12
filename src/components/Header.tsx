8d35abdc08e4e3326c996d0902b9d7676aed0e68
sherland <vancklies@gmail.com>
2023-08-12 03:08:09 +0300

    начато бургер меню

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Container, Row, Col,  } from 'react-bootstrap'
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import { Container, Row, Col, Navbar, Nav,  } from 'react-bootstrap'
 
 export default function Header() {
     return (
@@ -9,17 +7,21 @@ export default function Header() {
             <Row className='header__content'>
                 <Col className='header__navbar' sm='4'>
                     <Navbar className="">
-                        <Navbar.Toggle aria-controls="navbar-nav" />
-                        <Navbar.Collapse id="navbar-nav">
-                            <Nav className='header__nav'>
-                                <Nav.Link href='#makeOrder'>Цены</Nav.Link>
-                                <Nav.Link href='#example'>Примеры</Nav.Link>
-                                <Nav.Link href='#conditions'>Требования к макетам</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
+                        <Container fluid>
+                            <Navbar.Toggle aria-controls="navbar-nav" />
+                            <Navbar.Collapse id="navbar-nav">
+                                <Nav className='header__nav'>
+                                    <Nav.Link href='#makeOrder'>Цены</Nav.Link>
+                                    <Nav.Link href='#example'>Примеры</Nav.Link>
+                                    <Nav.Link href='#conditions'>Требования к макетам</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
                     </Navbar>
                 </Col>
-                <Col className='header__title align-self-center text-center' sm='4'>palette_print</Col>
+
+                <Col className='header__title align-self-center' sm='4'>palette_print</Col>
+
                 <Col className='header__contacts' sm='2'>
                     <ul className=''>
                         <li>+123 356 78 677</li>
@@ -27,14 +29,21 @@ export default function Header() {
                         <li>Пн-птн: 10:00 - 20:00	</li>
                     </ul>
                 </Col>
+
                 <Col className="header__button align-self-center" sm='2'>
                     <a href='#makeOrder'>Оформить заказ</a>
                 </Col>
+
+                <Col className='header__burger-btn' sm='8'><img src="images/burger-button.svg" alt=""/></Col>
             </Row>
             <Row className='header__text'>
-                <Col md='7' sm='12' className='header__text__title mb-5'>ТИПОГРАФИЯ ПОЛНОГО ЦИКЛА</Col>
-                <Col sm='10' className='header__text__desc'>Мы специализируемся на печати книг, брошюр и альбомов. Мы следим за качеством технического оснащения типографии, выполняем все стадии печатных работ.</Col>
+                <Col md='7' sm='12' className='header__text-title mb-5'>ТИПОГРАФИЯ ПОЛНОГО ЦИКЛА</Col>
+                <Col sm='10' className='header__text-subTitle'>
+                    Мы специализируемся на печати книг, брошюр и альбомов. Мы следим за качеством технического оснащения типографии, выполняем все стадии печатных работ.
+                </Col>
             </Row>
+
+            <div className='header__burger'></div>
         </Container>
     )
 }
